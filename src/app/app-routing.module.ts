3ee5e736b9437c71cce9c6e4ba51f59cbdde5343
ThakurAnupSingh7 <singhanup9412@gmail.com>
2019-08-13 13:34:00 +0530

    just-testing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { AbouUsComponent } from './abou-us/abou-us.component';
 import { GameInfoComponent } from './game-info/game-info.component';
 import { HomeComponent } from './home/home.component';
+import { OrganisationComponent } from './organisation/organisation.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'organisation',component:OrganisationComponent},
   {path:'about',component:AbouUsComponent},
   {path:'game-info',component:GameInfoComponent},
   {path:'home',component:HomeComponent}
