b3924ed7c788282715e69d798ced561d7d3eec15
ThakurAnupSingh7 <singhanup9412@gmail.com>
2019-08-31 01:02:11 +0530

    error-solve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,7 +37,7 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
     ParallaxDirective,
     FooterComponent,
 
-    PartnerComponent
+    PartnerComponent,
     OrganisationComponent,
     AdvisoryBoardComponent,
 
