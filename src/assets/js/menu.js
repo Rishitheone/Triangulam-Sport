ab4df27e61934e018284e6be1c4a5e146bfd8c64
ThakurAnupSingh7 <singhanup9412@gmail.com>
2019-09-02 17:13:10 +0530

    mobile view

--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,3 +1,6 @@
+
+//Search//
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "100%";
     document.getElementById("main").style.marginLeft = "250px";
@@ -8,3 +11,19 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+////////////////////
+
+
+
+//side menu on small screen////
+
+function openNavbar() {
+    document.getElementById("mySidenavbar").style.width = "100%";
+  }
+  
+  function closeNavbar() {
+    document.getElementById("mySidenavbar").style.width = "0";
+  }
+
+//////////////////////////////
